b6079096c26899cfcd357c42c4ce358731d73211
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-01-16 21:13:06 +0200

    added redis

---
+++ src/main/java/com/example/datastoremicroservice/config/RedisSchema.java
@@ -0,0 +1,18 @@
+package com.example.datastoremicroservice.config;
+
+import com.example.datastoremicroservice.model.MeasurementType;
+
+public class RedisSchema {
+
+    public static String sensorKeys() {
+        return KeyHelper.getKey("sensors");
+    }
+
+    public static String summaryKey(
+            long sensorId,
+            MeasurementType measurementType
+    ) {
+        return KeyHelper.getKey("sensors" + sensorId + ":" + measurementType.name().toLowerCase());
+    }
+
+}
---
+++ src/main/java/com/example/datastoremicroservice/service/impl/SummaryServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.datastoremicroservice.service.impl;
+
+import com.example.datastoremicroservice.model.MeasurementType;
+import com.example.datastoremicroservice.model.Summary;
+import com.example.datastoremicroservice.model.SummaryType;
+import com.example.datastoremicroservice.model.exception.SensorNotFoundException;
+import com.example.datastoremicroservice.repository.SummaryRepository;
+import com.example.datastoremicroservice.service.SummaryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class SummaryServiceImpl implements SummaryService {
+
+    private final SummaryRepository summaryRepository;
+
+    @Override
+    public Summary get(
+            Long sensorId,
+            Set<MeasurementType> measurementTypes,
+            Set<SummaryType> summaryTypes
+    ) {
+        return summaryRepository.findBySensorId(
+                        sensorId,
+                        measurementTypes == null ? Set.of(MeasurementType.values()) : measurementTypes,
+                        summaryTypes == null ? Set.of(SummaryType.values()) : summaryTypes
+                )
+                .orElseThrow(SensorNotFoundException::new);
+    }
+
+}
